5da317a6a72b63ba82000c85d1f867e89d771a14
tjhoe23 <125609687+tjhoe23@users.noreply.github.com>
2023-03-02 19:13:10 -0600

    Started script

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// TODO: add code here
+document.addEventListener("load", () => {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json", {
+        method: 'GET'
+    }).then((response) => {
+        response.json().then((jsonResponse) => {
+            console.log(jsonResponse)
+        })
+    response.json().then(i => i.forEach(i => console.log(i.name)))
+    }).catch((err) => {
+        console.log(`Error: ${err}`)
+    })
+    
+    
+})
+    
